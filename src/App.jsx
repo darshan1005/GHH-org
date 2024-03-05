1161c45ad6db76915569fc64fd9f4224e33792f9
darshan1005 <battuladarshan7838@gmail.com>
2024-03-06 00:56:18 +0530

    Demo updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import Nav from './Components/Nav-bar/Nav.jsx'
 import Hero from './Components/Hero/hero.jsx'
-import Top from './Components/Top-button/top.jsx'
-import Aboutus from './Components/About/about.jsx'
-import Ourwork from './Components/Ourwork/ourwork.jsx'
-import Gallery from './Components/Gallery/gallery.jsx'
-import Footer from './Components/Footer/footer.jsx'
+// import Top from './Components/Top-button/top.jsx'
+// import Aboutus from './Components/About/about.jsx'
+// import Ourwork from './Components/Ourwork/ourwork.jsx'
+// import Gallery from './Components/Gallery/gallery.jsx'
+// import Footer from './Components/Footer/footer.jsx'
 
 const App = () => {
   return (
     <>
       <Nav />
       <Hero />
-      <Top />
+      {/* <Top />
       <Aboutus />
       <Ourwork />
       <Gallery />
-      <Footer />
+      <Footer /> */}
     </>
   )
 }
