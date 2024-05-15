58dc788ddfa9a909efbdc3bb50a77b92f382af21
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2024-05-15 12:37:57 +0530

    Add files via upload

---
+++ src/Components/Instructions/Instruct.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Instruct = () =>{
+  return (
+    <>
+      <div className="instruct-div">
+        <h4 className="instruct-title">Read me</h4>
+        <p className="instruct">
+        We at <strong>Grace Helping Hands</strong> are dedicated to providing as much support as we can. If you need assistance, please don't hesitate to reach out to us. However, we kindly ask that you use our resources responsibly.
+        <br />
+        <br />
+        Our <b>services</b> are always available to you. Please use the contact details below solely for the purpose of seeking help. Misuse of this information is <mark>prohibited</mark>.
+        <br />
+        <br />
+        <i>We are here to help you.</i>
+        <br />
+        <strong>Grace Helping Hands</strong>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Instruct;
