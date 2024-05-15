9f752777928f0b2853f3e53aa37aab14167f5d4b
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2024-05-15 12:38:40 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Components/Hero/hero.jsx'
 import Top from './Components/Top-button/top.jsx'
 import Aboutus from './Components/About/about.jsx'
 import Ourwork from './Components/Ourwork/ourwork.jsx'
+import Instruct from './Components/Instructions/Instruct.jsx'
 import Footer from './Components/Footer/footer.jsx'
 import GridImages from './Components/Grid-gallery/grid-images.jsx'
 
@@ -14,6 +15,7 @@ const App = () => {
       <Aboutus />
       <Ourwork />
       <GridImages />
+      <Instruct />
       <Footer />
       <Top />
     </>
