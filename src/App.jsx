854868d4c720178f9778f2e1f3e706a42de2b41d
darshan1005 <battuladarshan7838@gmail.com>
2024-12-09 02:19:47 +0530

    GHH-MIG-d-Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import Components
-import Nav from "./Components/Nav-bar/Nav.jsx";
 import Hero from "./Components/Hero/hero.jsx";
 import Top from "./Components/Top-button/top.jsx";
 import Aboutus from "./Components/About/about.jsx";
 import Instruct from "./Components/Instructions/Instruct.jsx";
 import Footer from "./Components/Footer/footer.jsx";
+import Nav from "./Components/Nav-bar/Nav.jsx";
 import GridImages from "./Components/Grid-gallery/grid-images.jsx";
 import { TimeLine } from "./Components/TimeLine/timeLine.jsx";
 import { Stories } from "./Components/Stories/stories.jsx";
@@ -14,50 +14,27 @@ import { Stories } from "./Components/Stories/stories.jsx";
 const App = () => {
   return (
     <Router>
+      <Nav />
+      <Top />
       <Routes>
         {/* Home Page */}
         <Route
           path="/"
           element={
             <>
-              <Nav />
               <Hero />
               <Aboutus />
               <Footer />
               <Instruct />
-              <Top />
             </>
           }
         />
-
         {/* Gallery Page */}
-        <Route
-          path="/gallery"
-          element={
-            <>
-              <Nav />
-              <GridImages />
-            </>
-          }
-        />
-        <Route
-          path="/timeLine"
-          element={
-            <>
-              <Nav />
-              <TimeLine />
-            </>
-          }
-        />
-        <Route
-          path="/stories"
-          element={
-            <>
-              <Nav />
-              <Stories />
-            </>
-          }
-        />
+        <Route path="/gallery" element={<GridImages />} />
+        {/* TimeLine Page */}
+        <Route path="/timeLine" element={<TimeLine />} />
+        {/* Stories Page */}
+        <Route path="/stories" element={<Stories />} />
       </Routes>
     </Router>
   );
