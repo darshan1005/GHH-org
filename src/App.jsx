348c20ef814e779ac03edcfbff7fcb6e8d4399b1
darshan1005 <battuladarshan7838@gmail.com>
2024-12-10 00:46:54 +0530

    GHH-App-imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Import Components
 import { Hero } from "./Components/Hero/hero.jsx";
-import { About } from "./Components/About/About.jsx";
+import { About } from "./Components/About/about.jsx";
 import { Instruct } from "./Components/Instructions/Instruct.jsx";
 import { Footer } from "./Components/Footer/footer.jsx";
 import { Nav } from "./Components/Nav-bar/Nav.jsx";
 import { GridImages } from "./Components/Grid-gallery/grid-images.jsx";
-import { TimeLine } from "./Components/TimeLine/TimeLine.jsx";
+import { TimeLine } from "./Components/TimeLine/timeLine.jsx";
 import { Stories } from "./Components/Stories/stories.jsx";
 import { SingelStory } from "./Components/Stories/singleStory.jsx";
 import { TopButton } from "./Components/Top-button/TopButton.jsx";
