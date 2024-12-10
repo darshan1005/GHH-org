352266f7c3c819d166ae4e5bd51e058f590b8d23
darshan1005 <battuladarshan7838@gmail.com>
2024-12-11 00:05:43 +0530

    GHH-App import changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "./Components/Hero/Hero.jsx";
-import Instruct from "./Components/Instructions/Instruct.jsx";
-import Nav from "./Components/Nav-bar/Nav.jsx";
-import GridImages from "./Components/Grid-gallery/GridImages.jsx";
-import Stories from "./Components/Stories/Stories.jsx";
-import SingelStory from "./Components/Stories/SingleStory.jsx";
-import TopButton from "./Components/Top-button/TopButton.jsx";
-import About from "./Components/About/About.jsx";
-import Footer from "./Components/Footer/Footer.jsx";
-import TimeLine from "./Components/TimeLine/TimeLine.jsx";
+import { Hero } from "./Components/Hero/Hero.jsx";
+import { Instruct } from "./Components/Instructions/Instruct.jsx";
+import { Nav } from "./Components/Nav-bar/Nav.jsx";
+import { GridImages } from "./Components/Grid-gallery/GridImages.jsx";
+import { Stories } from "./Components/Stories/Stories.jsx";
+import { SingelStory } from "./Components/Stories/SingleStory.jsx";
+import { TopButton } from "./Components/Top-button/TopButton.jsx";
+import { About } from "./Components/About/About.jsx";
+import { Footer } from "./Components/Footer/Footer.jsx";
+import { TimeLine } from "./Components/TimeLine/TimeLine.jsx";
 
 const App = () => {
   return (
